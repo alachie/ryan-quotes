75696865dbe13145ef4a48d16d45695275f6e18f
alachie <_@lachie.co>
2019-02-23 18:36:48 +1100

    styles & multiple themes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,116 @@
 import React, { Component } from 'react';
 import fetch from 'unfetch';
+import styled, {css} from 'styled-components';
+import Lockr from 'lockr';
 
 function parseQuotes(text) {
   return text.replace(/[0-9]+-/gm, '').split('\n');
 }
+const AppStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background: #fff;
+  background: var(--bg-color);
+  color: #000;
+  color: var(--fg-color);
+  div {
+    flex: 1;
+  }
+
+  ${props => (props.theme === "dark") && css`
+    --bg-color: #101010;
+    --fg-color: #fff;
+    --border-color: #2f2f2f;
+  ` }
+`
+
+const HeaderStyled = styled.header`
+  display: flex;
+  justify-content: space-between;
+  margin: 2rem;
+  padding-bottom: 2rem;
+  border-bottom: 2px solid #eeeeee;
+  border-color: var(--border-color);
+  font-size: 2rem;
+  align-items: center;
+
+  h1 {
+    font-size: 2rem;
+    text-transform: uppercase;
+  }
+  a {
+    font-size: 2rem;
+    text-transform: uppercase;
+    font-weight: bold;
+    text-decoration: none;
+  }
+  .themeIcon {
+    padding-right: 2rem;
+    margin-right: 2rem;
+    border-right: 2px solid #eee;
+    border-color: var(--border-color);    
+    cursor: pointer;
+    user-select: none;
+  }
+
+  @media(max-width: 500px) {
+    h1, a {
+      font-size: 1.5rem;
+    }
+  }
+`
+
+const FooterStyled = styled.footer`
+  border-top: 2px solid #eeeeee;
+  border-color: var(--border-color);
+  margin: 2rem;
+  margin-top: 0;
+  font-size: 1rem;
+  padding-top: 1rem;
+  display: flex;
+  justify-content: space-between;
+`
+
+const ButtonStyled = styled.button`
+  margin: 2rem;
+  background: #fff;
+  background: var(--bg-color);
+  border: none;
+  font-size: 8rem;
+  padding: 8rem;
+  outline: none;
+  cursor: pointer;
+
+  &:active {
+    background: rgba(0,0,0, 0.5);
+  }
+`
+
+const QuoteNumber = styled.p`
+  text-align: center;
+  font-size: 2rem;
+  margin-bottom: 4rem;
+  @media(max-width: 900px) {
+    font-size: 1.5rem;
+  }
+`
+
+const QuoteText = styled.blockquote`
+  text-align: center;
+  font-size: 4rem;
+  max-width: 700px;
+  padding: 0 2rem;
+  @media(max-width: 900px) {
+    font-size: 2rem;
+  }
+`
+const QuoteWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 class App extends Component {
   constructor() {
@@ -11,12 +118,25 @@ class App extends Component {
     this.state = {
       isLoading: true,
       quotes: [],
-      currentQuote: ""
+      currentQuote: '',
+      currentQuoteId: '',
+      theme: 'dark',
     }
     this.getRandomQuote = this.getRandomQuote.bind(this);
+    this.toggleTheme    = this.toggleTheme.bind(this);
+
+  }
+
+  componentWillMount() {
+    if (Lockr.get('theme')) {
+      this.setState({
+        theme: Lockr.get('theme')
+      })
+    }
   }
 
   componentDidMount() {
+
     // load quotes
     fetch('/quotes.txt')
       .then(text => text.text())
@@ -31,21 +151,51 @@ class App extends Component {
   }
 
   getRandomQuote() {
-
     const { quotes } = this.state;
+    const id = Math.floor(Math.random() * quotes.length);
+
     this.setState({
-      currentQuote: quotes[Math.floor(Math.random() * quotes.length)]
+      currentQuote: quotes[id],
+      currentQuoteId: id.toString(),
     })
   }
 
+  toggleTheme(e) {
+    e.preventDefault();
+    
+    const theme = (this.state.theme === "light" ? 'dark' : 'light');
+
+    this.setState({
+      theme
+    });
+
+    Lockr.set('theme', theme);
+  }
+
   render() {
+    const { isLoading, currentQuoteId, currentQuote, theme} = this.state;
+
     return (
-      <div className="App">
-        {this.state.isLoading ? <div>loading...</div> : <div>
-          <p>{this.state.currentQuote}</p>
-          <button onClick={this.getRandomQuote}>🎲</button>
-        </div>}
-      </div>
+      <AppStyled theme={theme}>
+        {isLoading ? <div>loading...</div> : <React.Fragment>
+          <HeaderStyled>
+            <h1>Ryanemzed Quotes</h1>
+            <span>
+              <a href="#" className="themeIcon" onClick={this.toggleTheme}>{(theme === "light" ? '🌚' : '🌝')}</a>
+              <a href="https://www.twitch.tv/ryanemzed">Twitch &rarr;</a>
+            </span>
+          </HeaderStyled>
+          <QuoteWrapper>
+            <QuoteNumber>Quote #{currentQuoteId}</QuoteNumber>
+            <QuoteText>{currentQuote}</QuoteText>
+          </QuoteWrapper>
+          <ButtonStyled onClick={this.getRandomQuote}>🎲</ButtonStyled>
+          <FooterStyled>
+            <span>© {new Date().getFullYear()}</span>
+            <span>View on <a href="https://github.com/alachie/ryan-quotes" target="_blank" rel="noopener noreferrer" >GitHub</a></span>
+          </FooterStyled>
+        </React.Fragment>}
+      </AppStyled>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createGlobalStyle} from 'styled-components';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        box-sizing: inherit;
+    }
+
+    html {
+        --bg-color: #fff;
+        --fg-color: #000;
+        --border-color: #eee;
+    }
+
+    html {
+        font-size: 10px;
+        background: #fff;
+        background: var(--bg-color);
+        box-sizing: border-box;
+    }
+
+    html, body, h1, h2, h3, h4, h5, h6, p, blockquote {
+        margin: 0;
+        padding: 0;
+    }
+
+    a {
+        color: black;
+        color: var(--fg-color);
+    }
+
+    body {
+        font-family: sans-serif;
+        font-size: 3rem;
+        font-family: -apple-system, BlinkMacSystemFont, "Helvetica Neue", Roboto, sans-serif;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        color: var(--fg-color);
+    }
+`
+
+ReactDOM.render(
+    <React.Fragment>
+        <GlobalStyle/>
+        <App />
+    </React.Fragment>
+, document.getElementById('root'));
